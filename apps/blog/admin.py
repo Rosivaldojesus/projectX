4afa4ece3b7d5df06347b2b0135be3114eda83fa
Rosivaldojesus <js.rosivaldo@gmail.com>
2021-04-20 00:21:40 -0300

    exluir summerNote

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 from .models import Categoria, Post, Comentario
 
-#Importando o sumerNote
-from django_summernote.admin import SummernoteModelAdmin
+
 
 # Register your models here.
 
--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path, include
 from . import views
 
-from django.conf import settings
-from django.conf.urls.static import static
-
 
 urlpatterns = [
 
@@ -15,5 +12,4 @@ urlpatterns = [
 
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
